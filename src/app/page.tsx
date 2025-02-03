00debe8460375409cec0b0f3d79fe799a1b151d2
AtulMinz <atulminz.jsr@gmail.com>
2025-02-03 11:07:09 +0000

    Merged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Hero from "./pages/Hero";
 
 export default function Page() {
   return (
-    <>
+    <main className="h-[100vh] w-[100vw]">
       <Hero />
-    </>
+    </main>
   );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Navbar() {
+  return (
+    <nav className="pb-10 p-4 w-[100%] flex items-start border-b border-gray-200 border-opacity-50">
+      <h1 className="">Navbar</h1>
+    </nav>
+  );
+}
