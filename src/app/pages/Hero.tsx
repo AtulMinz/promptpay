6144e3ba28a843a7a3f99f074e5444ba23b6a7f8
AtulMinz <atulminz.jsr@gmail.com>
2025-02-05 11:07:04 +0000

    Updated new design

--- src/app/pages/Hero.tsx
+++ src/app/pages/Hero.tsx
@@ -11,8 +11,11 @@ export default function Home() {
       <motion.div className="bg-gray-950 w-[100vw] h-[100vh] flex flex-col justify-center items-center">
         <Navbar />
         <motion.div className="flex flex-col justify-center items-center">
-          <motion.div className="flex justify-center items-center">
-            <TextScramble className="font-extrabold text-6xl text-slate-200 font-serif">
+          <motion.div className="flex justify-center items-center bg-transparent flex-col">
+            <TextScramble className="font-extrabold text-6xl text-slate-300">
+              Sendr
+            </TextScramble>
+            <TextScramble className="font-extrabold text-4xl text-slate-300 font-sans">
               Create your web3 friend
             </TextScramble>
           </motion.div>
